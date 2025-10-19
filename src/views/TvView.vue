734469ee19bd715d87d72459a0c37f34db8fca5b
iamayara <iammayaraifc@gmail.com>
2025-10-19 15:18:26 -0300

    FEAT: ícon carregando e store-tv

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,12 +1,17 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import api from '@/plugins/axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
+import { useGenreStore } from '@/stores/genre';
 
-const genres = ref([]);
+const genreStore = useGenreStore();
+const isLoading = ref(false);
 const shows = ref([]);
 
 const listShows = async (genreId) => {
   try {
+    isLoading.value = true;
     const response = await api.get('discover/tv', {
       params: {
         with_genres: genreId,
@@ -16,15 +21,21 @@ const listShows = async (genreId) => {
     shows.value = response.data.results;
   } catch (error) {
     console.error('Erro ao buscar programas de TV:', error);
+  } finally {
+    isLoading.value = false;
   }
 };
 
+const formatDate = (date) => new Date(date).toLocaleDateString('pt-BR');
+
 onMounted(async () => {
   try {
-    const response = await api.get('genre/tv/list?language=pt-BR');
-    genres.value = response.data.genres;
+    isLoading.value = true;
+    await genreStore.getAllGenres('tv');
   } catch (error) {
     console.error('Erro ao buscar gêneros de TV:', error);
+  } finally {
+    isLoading.value = false;
   }
 });
 </script>
@@ -34,7 +45,7 @@ onMounted(async () => {
     <h1>Programas de TV</h1>
     <ul class="genre-list">
       <li
-        v-for="genre in genres"
+        v-for="genre in genreStore.genres"
         :key="genre.id"
         @click="listShows(genre.id)"
         class="genre-item"
@@ -42,6 +53,7 @@ onMounted(async () => {
         {{ genre.name }}
       </li>
     </ul>
+    <loading v-model:active="isLoading" is-full-page />
     <div class="tv-list">
       <div v-for="show in shows" :key="show.id" class="tv-card">
         <img
@@ -49,42 +61,54 @@ onMounted(async () => {
           :src="`https://image.tmdb.org/t/p/w500${show.poster_path}`"
           :alt="show.name"
         />
+
         <div class="tv-details">
           <p class="tv-title">{{ show.name }}</p>
           <p class="tv-original-name">({{ show.original_name }})</p>
-          <p class="tv-date">{{ show.first_air_date }}</p>
+          <p class="tv-date">{{ formatDate(show.first_air_date) }}</p>
+
+          <p class="tv-genres">
+            <span
+              v-for="genre_id in show.genre_ids"
+              :key="genre_id"
+              @click="listShows(genre_id)"
+              class="genre-tag"
+            >
+              {{ genreStore.getGenreName(genre_id) }}
+            </span>
+          </p>
         </div>
       </div>
     </div>
   </div>
 </template>
 <style scoped>
-  .genre-list {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    gap: 2rem;
-    list-style: none;
-    padding: 0;
-  }
 
-  .genre-item {
-    background-color: #5d6424;
-    border-radius: 1rem;
-    padding: 0.5rem 1rem;
-    align-self: center;
-    color: #fff;
-    display: flex;
-    justify-content: center;
-  }
+.genre-list {
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+  list-style: none;
+  padding: 0;
+}
 
-  .genre-item:hover {
-    cursor: pointer;
-    background-color: #7d8a2e;
-    box-shadow: 0 0 0.5rem #5d6424;
-  }
+.genre-item {
+  background-color: #5d6424;
+  border-radius: 1rem;
+  padding: 0.5rem 1rem;
+  align-self: center;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+}
 
-  .tv-list {
+.genre-item:hover {
+  cursor: pointer;
+  background-color: #7d8a2e;
+  box-shadow: 0 0 0.5rem #5d6424;
+}
+.tv-list {
   display: flex;
   flex-wrap: wrap;
   gap: 1rem;
@@ -114,5 +138,28 @@ onMounted(async () => {
   font-weight: bold;
   line-height: 1.3rem;
   height: 3.2rem;
+}
+.tv-genres {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 0.2rem;
+}
+
+.tv-genres span {
+  background-color: #748708;
+  border-radius: 0.5rem;
+  padding: 0.2rem 0.5rem;
+  color: #fff;
+  font-size: 0.8rem;
+  font-weight: bold;
+}
+
+.tv-genres span:hover {
+  cursor: pointer;
+  background-color: #455a08;
+  box-shadow: 0 0 0.5rem #748708;
 }
 </style>
