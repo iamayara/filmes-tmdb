3524ca321dabcd9f250c77538a1e1ae52a644f63
iamayara <iammayaraifc@gmail.com>
2025-10-19 14:43:42 -0300

    FEAT: filtragem programas de tv

--- src/views/TvView.vue
+++ src/views/TvView.vue
@@ -1,24 +1,63 @@
 <script setup>
-  import { ref, onMounted } from 'vue';
-  import api from '@/plugins/axios';
+import { ref, onMounted } from 'vue';
+import api from '@/plugins/axios';
 
-  const genres = ref([]);
+const genres = ref([]);
+const shows = ref([]);
 
-  onMounted(async () => {
+const listShows = async (genreId) => {
+  try {
+    const response = await api.get('discover/tv', {
+      params: {
+        with_genres: genreId,
+        language: 'pt-BR',
+      },
+    });
+    shows.value = response.data.results;
+  } catch (error) {
+    console.error('Erro ao buscar programas de TV:', error);
+  }
+};
+
+onMounted(async () => {
+  try {
     const response = await api.get('genre/tv/list?language=pt-BR');
     genres.value = response.data.genres;
-  });
+  } catch (error) {
+    console.error('Erro ao buscar gêneros de TV:', error);
+  }
+});
 </script>
 
 <template>
-  <h1>Programas de TV</h1>
-  <ul class="genre-list">
-    <li v-for="genre in genres" :key="genre.id" class="genre-item">
-      {{ genre.name }}
-    </li>
-  </ul>
+  <div>
+    <h1>Programas de TV</h1>
+    <ul class="genre-list">
+      <li
+        v-for="genre in genres"
+        :key="genre.id"
+        @click="listShows(genre.id)"
+        class="genre-item"
+      >
+        {{ genre.name }}
+      </li>
+    </ul>
+    <div class="tv-list">
+      <div v-for="show in shows" :key="show.id" class="tv-card">
+        <img
+          v-if="show.poster_path"
+          :src="`https://image.tmdb.org/t/p/w500${show.poster_path}`"
+          :alt="show.name"
+        />
+        <div class="tv-details">
+          <p class="tv-title">{{ show.name }}</p>
+          <p class="tv-original-name">({{ show.original_name }})</p>
+          <p class="tv-date">{{ show.first_air_date }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
 <style scoped>
   .genre-list {
     display: flex;
@@ -44,4 +83,36 @@
     background-color: #7d8a2e;
     box-shadow: 0 0 0.5rem #5d6424;
   }
+
+  .tv-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+}
+
+.tv-card {
+  width: 15rem;
+  height: 30rem;
+  border-radius: 0.5rem;
+  overflow: hidden;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-card img {
+  width: 100%;
+  height: 20rem;
+  border-radius: 0.5rem;
+  box-shadow: 0 0 0.5rem #000;
+}
+
+.tv-details {
+  padding: 0 0.5rem;
+}
+
+.tv-title {
+  font-size: 1.1rem;
+  font-weight: bold;
+  line-height: 1.3rem;
+  height: 3.2rem;
+}
 </style>
